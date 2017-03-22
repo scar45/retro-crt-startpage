14088b61645ef99a72bdcc70a3c2c21d80f2467a
George Merlocco <george@merloc.co>
2017-03-22 20:40:42 +0100

    Refactoring for Node/SASS live development

---
+++ README.md
@@ -0,0 +1,54 @@
+# Retro CRT Startpage
+#### by scar45/George Merlocco @ https://scar45.me
+
+---
+
+This repository contains an HTML5-based layout for creating a personalized startpage with a retro CRT feel. More information will be added to this file as I progress.
+
+[Browsersync](http://www.browsersync.io) is used for live coding (browser-sync) which vastly accelerates development time.
+
+## Requirements
+
+- [Node.js](http://nodejs.org)
+
+## Getting it running
+
+**Install dependencies:**
+```
+npm install
+```
+### Developing
+
+**Start a first build, then spawn webserver for live coding (browser-sync):**
+```
+gulp
+```
+- This will dump compiled/processed files in a ./build directory, which will then be served by browser-sync, with files being watched for changes. When changes occur, browser-sync will automagically refresh the browser.
+
+### Releasing
+
+**Run ```gulp``` with a parameter of ```release``` which will clean the ```./build``` directory and recompile all sources fresh**:
+
+```
+gulp release
+```
+
+### Running under Node http-server
+
+```
+npm install http-server -g
+```
+
+then:
+
+```
+http-server
+```
+
+You'll find everything in ```./build``` again, ready to be served from your preferred webserver. 
+
+---
+
+...and that's about it! Thanks!
+
+_.end_
---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    concat = require("gulp-concat"),
+    del = require('del'),
+	minifyHTML = require("gulp-minify-html"),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    stripCssComments = require('gulp-strip-css-comments'),
+    runSequence = require('run-sequence');
+	
+var config = {
+    paths: {
+        html: {
+            src:  "src/**/*.html",
+            dest: "build"
+        },
+        json: {
+            src:  "src/json/*.json",
+            dest: "build/json"
+        },
+        javascript: {
+            src:  "src/js/**/*.js",
+            dest: "build/js"
+        },
+        images: {
+            src: ["src/images/**/*.jpg", "src/images/**/*.jpeg", "src/images/**/*.png", "src/images/**/*.svg"],
+            dest: "build/images"
+        },
+        css: {
+            src: ["src/css/normalize.css", "src/css/whhg.css"],
+            dest: "build/css"
+        },
+        sass: {
+            src: ["src/sass/**/*.scss"],
+            dest: "build/css"
+        },
+        untouched: {
+            src: ["src/.htaccess", "src/*.png", "src/*.xml", "src/*.txt"],
+            dest: "build",
+            fontSrc: "src/fonts/**/*",
+            fontDest: "build/fonts"
+        }
+    }
+};
+
+gulp.task('clean', function() {
+    return del('./build');
+});
+
+gulp.task('cleanpostrelease', function() {
+    return del(['./build/css/**/*.map', './build/css/**/*.css' ]);
+});
+
+gulp.task('html', ['scripts'], function() {
+    return gulp.src(config.paths.html.src)
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(config.paths.html.dest));
+});
+
+gulp.task('css', ['sass'], function () {
+    return gulp.src(config.paths.css.src)
+        .pipe(concat("startpage-helpers.css"))
+        .pipe(stripCssComments())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(config.paths.css.dest))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(config.paths.sass.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.paths.sass.dest))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+    gulp.src(config.paths.javascript.src)
+        .pipe(gulp.dest(config.paths.javascript.dest));
+    gulp.src("./node_modules/vanilla-modal/dist/index.js")
+        .pipe(rename("vanilla-modal.js"))
+        .pipe(gulp.dest("./build/js/vendor"));
+});
+
+gulp.task("untouched", function(){
+    gulp.src(config.paths.untouched.src)
+        .pipe(gulp.dest(config.paths.untouched.dest));
+    gulp.src(config.paths.untouched.fontSrc)
+        .pipe(gulp.dest(config.paths.untouched.fontDest));
+	gulp.src(config.paths.images.src)
+		.pipe(gulp.dest(config.paths.images.dest));
+    gulp.src(config.paths.json.src)
+    .pipe(gulp.dest(config.paths.json.dest));
+});
+
+gulp.task('liveCoding', ['untouched', 'html', 'css'], function() {
+    // Quick 2s delay to allow all tasks to complete
+    setTimeout(function() {
+        browserSync({
+            server: {
+                baseDir: './build'
+            },
+            reloadDelay: 1000
+        });
+    }, 2000);
+
+});
+
+gulp.task('release', function() {
+    runSequence('clean', 'css', ['untouched', 'html'], 'cleanpostrelease')
+});
+
+gulp.task('default', ['liveCoding'], function(){
+    gulp.watch(config.paths.html.src, ['html', browserSync.reload]);
+    gulp.watch(config.paths.css.src, ['css', browserSync.reload]);
+    gulp.watch(config.paths.sass.src, ['sass', browserSync.reload]);
+    gulp.watch(config.paths.javascript.src, ['scripts', browserSync.reload]);
+    gulp.watch(config.paths.images.src, ['images', browserSync.reload]);
+    gulp.watch(config.paths.untouched.src, ['untouched', browserSync.reload]);
+});
