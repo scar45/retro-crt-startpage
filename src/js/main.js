c9e0f876df8a666acd52eea52d6e48c712f6acc1
George Merlocco <george@merloc.co>
2017-03-23 00:14:38 -0400

    Code cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@
 
 $(".surround").click(function(e) {
     if ($(e.currentTarget).hasClass("on")) {
-        $(".surround").addClass("on")
-        $(e.currentTarget).removeClass("on")
+        $(".surround").addClass("on");
+        $(e.currentTarget).removeClass("on");
         $("#switch").prop("checked", false);
     } else {
-        $(".surround").removeClass("on")
-        $(e.currentTarget).addClass("on")
+        $(".surround").removeClass("on");
+        $(e.currentTarget).addClass("on");
         $("#switch").prop("checked", true);
     }
-})
+});
