5428b467eed43316055726f3e85935619f6c829a
George Merlocco <george@merloc.co>
2017-03-23 21:01:21 +0100

    Adding flicker toggle

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-// Helper for toggling CRT screen power
 
+// Helper for toggling CRT screen power effect
 $(".surround").click(function(e) {
     if ($(e.currentTarget).hasClass("on")) {
         $(".surround").addClass("on");
@@ -9,5 +9,16 @@ $(".surround").click(function(e) {
         $(".surround").removeClass("on");
         $(e.currentTarget).addClass("on");
         $("#switch").prop("checked", true);
+    }
+});
+
+// Helper for toggling CRT screen flickering effect
+$(".flicker-button").click(function() {
+    if ($("#flicker").is(":checked")) {
+        $("#flicker").prop("checked", false);
+		$(".flicker-button").removeClass("on");
+    } else {
+        $("#flicker").prop("checked", true);
+		$(".flicker-button").addClass("on");
     }
 });
