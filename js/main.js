0734096f1b4a11bb4d7cc3624b67ec999578c292
George Merlocco <george@merloc.co>
2020-12-30 17:33:07 -0500

    Update demo to match latest changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 // Helper for toggling CRT screen power effect
-
-// Turn on the CRT monitor and enable scanlines if the flicker checkbox is enabled
 function powerOn() {
-/*    if ($("#flicker").is(":checked")) {
-        $(".crt-effects").addClass("scanlines");
-    }*/
+    var sndOn = document.getElementById("snd_power_on");
+    sndOn.play();
     $("#switch").prop("checked", true);
     $(".surround").addClass("on");
     createCookie('power', 1);
 }
-// Shut off the CRT monitor
 function powerOff() {
+    var sndOff = document.getElementById("snd_power_off");
+    sndOff.play();
     $("#switch").prop("checked", false);
     $(".surround").removeClass("on");
-    //$(".crt-effects").removeClass("scanlines");
     createCookie('power', 0);
+}
+function togglePower() {
+    if ($("#switch").prop("checked")) {
+        powerOff()
+    } else {
+        powerOn()
+    }
 }
 
 // Helper for toggling CRT screen flickering effect
@@ -30,6 +34,14 @@ function scanlinesOff() {
     $(".power-label").removeClass("btn-scanlines");
     createCookie('flicker',0);
 }
+function toggleScanlines() {
+    //if ($("#flicker").is(":checked") && $("#switch-wrap").hasClass("on")) {
+    if ($("#flicker").is(":checked")) {
+        scanlinesOff();
+    } else {
+        scanlinesOn();
+    }
+}
 
 // Helper for toggling CRT color theme
 function greenTheme() {
@@ -42,24 +54,6 @@ function amberTheme() {
     $("body").removeClass("green");
     createCookie('greenTheme',0);
 }
-
-function togglePower() {
-    if ($("#switch").prop("checked")) {
-        powerOff()
-    } else {
-        powerOn()
-    }
-}
-
-function toggleScanlines() {
-    //if ($("#flicker").is(":checked") && $("#switch-wrap").hasClass("on")) {
-    if ($("#flicker").is(":checked")) {
-        scanlinesOff();
-    } else {
-        scanlinesOn();
-    }
-}
-
 function toggleTheme() {
     if ($("#greenTheme").is(":checked")) {
         amberTheme();
@@ -83,7 +77,7 @@ function loadJSON() {
                 $(".shortcuts").append('' +
                     '<li class="tagged-item" data-item-tags="'+data.tags+'">' +
                     '<a class="bookmark" href="'+data.url+'" target="_blank">' +
-                    '<img class="retro invert" src="images/'+data.icon+'"/>' +
+                    '<img class="retro invert" src="images/icons/'+data.icon+'"/>' +
                     '<span class="link-name">'+data.name+'</span>' +
                     '<br><span class="link-url">'+data.url+'</span>' +
                     '<br><span class="tags"></span><br>' +
@@ -93,7 +87,7 @@ function loadJSON() {
                 $(".shortcuts").append('' +
                     '<li class="tagged-item" data-item-tags="'+data.tags+'">' +
                     '<a class="bookmark" href="'+data.url+'" target="_blank">' +
-                    '<img class="retro" src="images/'+data.icon+'"/>' +
+                    '<img class="retro" src="images/icons/'+data.icon+'"/>' +
                     '<span class="link-name">'+data.name+'</span>' +
                     '<br><span class="link-url">'+data.url+'</span>' +
                     '<br><span class="tags"></span><br>' +
@@ -101,22 +95,21 @@ function loadJSON() {
                 );
             }
             $.each(data.tags, function(t,tag){
-                //console.log(tag);
                 $(".tags").eq(i).append('<span>'+tag+'</span>');
             });
         });
     }).done(function(){
-        $('div.tag-list').tagSort({
+        $('div.tag-wrap').tagSort({
             items: 'li.tagged-item',
             reset: '.tagsort-reset',
-            fadeTime: 420
+            sortType: 'single',
+            fadeTime: 200
         });
-        $('div.tag-list span:not(.tagsort-reset)').sort(sortAlpha).appendTo('div.tag-list');
+        $('div.tag-wrap span:not(.tagsort-reset)').sort(sortAlpha).appendTo('div.tag-wrap');
     });
 }
 
-// Weather support via simpleWeather v3.1.0 - http://simpleweatherjs.com
-// Replace 'Toronto' with your city name
+// Unused location lookup to be used with weather status later on (darksky.com)
 function initLocation() {
     //console.log("Getting weather info...");
     if ("geolocation" in navigator) {
@@ -134,7 +127,6 @@ function initLocation() {
             console.log('Longitude: ' + crd.longitude);
             console.log('More or less ' + crd.accuracy + ' meters.');
 
-            loadWeather(pos.coords.latitude + ',' + pos.coords.longitude);
         };
 
         function error(err) {
@@ -143,33 +135,12 @@ function initLocation() {
 
         navigator.geolocation.getCurrentPosition(success, error, options);
     } else {
-        loadWeather('Toronto', ''); //@params location, woeid
+        // Manually input a city to pull weather data for
     }
 }
 
-function loadWeather(location, woeid) {
-    function update() {
-        $('#clock').html(moment().format('MM/DD/YY h:mm:ss a'));
-    }
-    $.simpleWeather({
-        location: location,
-        woeid: woeid,
-        unit: 'c',
-        success: function (weather) {
-            html = '<div class="weather-details"><ul class="weather-current"><li><a href="' + weather.link + '" target="_blank"><i class="icon-weather icon-' + weather.code + '"></i></a> ' + weather.temp + '&deg;' + weather.units.temp + '</li>';
-            html += '<li class="currently">' + weather.currently + '</li>';
-            html += '<li>H:' + weather.high + '&deg;C // L:' + weather.low + '&deg;C</li>';
-            html += '<li>' + weather.city + ', ' + weather.region + '</li></ul>';
-            html += '<ul class="weather-forecast"><li><i class="icon-weather icon-' + weather.forecast[1].code + '"></i><span>' + weather.forecast[1].day + ': ' + weather.forecast[1].text + '<br>H: ' + weather.forecast[1].high + '&deg;C // L: ' + weather.forecast[1].low + '&deg;C</span></li><li><i class="icon-weather icon-' + weather.forecast[2].code + '"></i><span>' + weather.forecast[2].day + ': ' + weather.forecast[2].text + '<br>H: ' + weather.forecast[2].high + '&deg;C // L: ' + weather.forecast[2].low + '&deg;C</span></li><li><i class="icon-weather icon-' + weather.forecast[3].code + '"></i><span>' + weather.forecast[3].day + ': ' + weather.forecast[3].text + '<br>H: ' + weather.forecast[3].high + '&deg;C // L: ' + weather.forecast[3].low + '&deg;C</span></li></ul></div>';
-            html += '<p><span id="clock"></span> // Weather updated ' + moment(weather.updated).fromNow() + '</p>';
-            $("#weather").html(html);
-            //console.log(html);
-            setInterval(update, 1000);
-        },
-        error: function (error) {
-            $("#weather").html('<p>' + error + '</p>');
-        }
-    });
+function updateTime() {
+    $('#clock').html(moment().format('MM/DD/YYYY h:mm:ss a'));
 }
 
 // Totally pointless, yet somewhat stylish extra 1KB of randomly generated pseudo-encryption bloat
@@ -217,7 +188,7 @@ function readPrefs() {
         cookieGreenTheme=readCookie('greenTheme');
 
     if (cookiePower == 0) { powerOff() } else { powerOn() }
-    if (cookieFlicker == 0) { scanlinesOff() } else { scanlinesOn() }
+    if (cookieFlicker == 1) { scanlinesOn() } else { scanlinesOff() }
     if (cookieGreenTheme == 0) { amberTheme() } else { greenTheme() }
 
     console.log("cookiePower=="+cookiePower+" /// cookieFlicker=="+cookieFlicker+" /// cookieGreenTheme=="+cookieGreenTheme);
@@ -225,12 +196,21 @@ function readPrefs() {
 
 $(document).ready(function () {
     readPrefs(); // Read site preferences (flicker, colour, etc.)
-    initLocation(); // Request location for weather via the browser
     loadJSON(); // Read the user's links collection
-    randomgen(); // Add 1KB to the page for that A E S T H E T I C look
-    setInterval(randomgen, 300000); // Regenerate the fake encrypted string in footer every 5 minutes because why not?
-    setInterval(initLocation, 600000); // Update the weather every 10 minutes.
+    setInterval(updateTime, 1000); // Update the time every second.
 
+    // Disabled functions (design change + weather API issues)
+    //randomgen(); // Add 1KB to the page for that A E S T H E T I C look
+    //setInterval(randomgen, 300000); // Regenerate the fake encrypted string in footer every 5 minutes because why not?
+    //initLocation(); // Request location for weather via the browser
+    //setInterval(initLocation, 600000); // Update the weather every 10 minutes.
+
+    // Toggle display of tags on smaller breakpoints
+    $(".tagsort-toggle").on('click', function() {
+        $(".tag-wrap").toggle();
+    });
+
+    // Design element toggles
     $(".surround").on('click', togglePower);
     $(".power-label").on('click',toggleScanlines);
     $(".theme-button").on('click', toggleTheme);
