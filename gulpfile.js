4c8708eca20488f0be85bebb115f0c0a60639876
George Merlocco <george@merloc.co>
2022-09-19 23:56:26 -0400

    Migrate to Gulp v4; Upgrade all NPM packages (except 'del')

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
+    browserSync = require('browser-sync').create(),
     concat = require("gulp-concat"),
     del = require('del'),
 	minifyHTML = require("gulp-minify-html"),
     minifyCSS = require('gulp-minify-css'),
     package = require('./package.json'),
     rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass')(require('node-sass')),
     sourcemaps = require('gulp-sourcemaps'),
     stripCssComments = require('gulp-strip-css-comments'),
     runSequence = require('run-sequence'),
@@ -63,39 +63,40 @@ gulp.task('ziprelease', function() {
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('html', ['scripts'], function() {
-    return gulp.src(config.paths.html.src)
-        .pipe(minifyHTML())
-        .pipe(gulp.dest(config.paths.html.dest));
-});
-
-gulp.task('css', ['sass'], function () {
-    return gulp.src(config.paths.css.src)
-        .pipe(concat("startpage-helpers.css"))
-        .pipe(stripCssComments())
-        .pipe(minifyCSS())
-        .pipe(gulp.dest(config.paths.css.dest))
-        .pipe(browserSync.reload({stream: true}));
-});
-
 gulp.task('sass', function () {
     return gulp.src(config.paths.sass.src)
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.paths.sass.dest))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function(done) {
     gulp.src(config.paths.javascript.src)
         .pipe(gulp.dest(config.paths.javascript.dest));
     gulp.src("./node_modules/vanilla-modal/dist/index.js")
         .pipe(rename("vanilla-modal.js"))
         .pipe(gulp.dest("./build/js/vendor"));
+    done();
 });
 
-gulp.task("untouched", function(){
+gulp.task('html', gulp.series('scripts', function() {
+    return gulp.src(config.paths.html.src)
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(config.paths.html.dest));
+}));
+
+gulp.task('css', gulp.series('sass', function () {
+    return gulp.src(config.paths.css.src)
+        .pipe(concat("startpage-helpers.css"))
+        .pipe(stripCssComments())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(config.paths.css.dest))
+        .pipe(browserSync.stream());
+}));
+
+gulp.task("untouched", function(done){
     gulp.src(config.paths.untouched.src)
         .pipe(gulp.dest(config.paths.untouched.dest));
     gulp.src(config.paths.untouched.fontSrc)
@@ -105,32 +106,46 @@ gulp.task("untouched", function(){
 	gulp.src(config.paths.images.src)
 		.pipe(gulp.dest(config.paths.images.dest));
     gulp.src(config.paths.json.src)
-    .pipe(gulp.dest(config.paths.json.dest));
+        .pipe(gulp.dest(config.paths.json.dest));
+    done();
 });
 
-gulp.task('liveCoding', ['untouched', 'html', 'css'], function() {
-    // Quick 2s delay to allow all tasks to complete
-    setTimeout(function() {
-        browserSync({
-            server: {
-                baseDir: './build'
-            },
-            reloadDelay: 1000
-        });
-    }, 2000);
+gulp.task('release', gulp.series('clean','untouched','html','css','cleanpostrelease','ziprelease'));
 
-});
+function serve(done) {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
+    done();
+}
 
-gulp.task('release', function() {
-    runSequence('clean', ['untouched', 'html', 'css'], 'cleanpostrelease', 'ziprelease')
-});
+const watchHTML = () => gulp.watch(config.paths.html.src, gulp.series('html', function (done) {
+    browserSync.reload();
+    done();
+}));
+const watchCSS = () => gulp.watch(config.paths.css.src, gulp.series('css',  function (done) {
+    browserSync.reload();
+    done();
+}));
+const watchSASS = () => gulp.watch(config.paths.sass.src, gulp.series('sass',  function (done) {
+    browserSync.reload();
+    done();
+}));
+const watchJS = () => gulp.watch(config.paths.javascript.src, gulp.series('scripts',  function (done) {
+    browserSync.reload();
+    done();
+}));
+const watchUntouched = () => gulp.watch(config.paths.untouched.src, gulp.series('untouched',  function (done) {
+    browserSync.reload();
+    done();
+}));
 
-gulp.task('default', ['liveCoding'], function(){
-    gulp.watch(config.paths.html.src, ['html', browserSync.reload]);
-    gulp.watch(config.paths.css.src, ['css', browserSync.reload]);
-    gulp.watch(config.paths.sass.src, ['sass', browserSync.reload]);
-    gulp.watch(config.paths.javascript.src, ['scripts', browserSync.reload]);
-    gulp.watch(config.paths.images.src, ['untouched', browserSync.reload]);
-    gulp.watch(config.paths.json.src, ['untouched', browserSync.reload]);
-    gulp.watch(config.paths.untouched.src, ['untouched', browserSync.reload]);
-});
+const liveCoding = gulp.series(
+    serve,
+    gulp.series('untouched', 'html', 'css'),
+    gulp.parallel(watchHTML,watchCSS,watchSASS,watchJS,watchUntouched)
+);
+
+exports.default = liveCoding;
